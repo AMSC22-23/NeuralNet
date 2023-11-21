f737aa7d332f25ed9b2df4b1d2bd1b3d343540db
FilippoIspanico <filippo.ispanico@gmail.com>
2023-11-21 17:09:03 +0000

    minor changes

--- FM/OpenBlasExperiments/main.cpp
+++ FM/OpenBlasExperiments/main.cpp
@@ -1,6 +1,32 @@
 #include <cblas.h>
 #include <iostream>
 
+// source /u/sw/etc/bash.bashrc 
+// module load gcc-glibc/11.2.0
+// module load openblas
+
+/*
+ This module provides an optimized implementation of the BLAS and LAPACK
+      libraries, it is both multithreaded and optimized for multiple platforms.
+      For using OpenBLAS library you should use the linker flags:
+        -L$mkOpenblasLib -lopenblas
+      By default the library run in single-thread mode, but you can set the number of
+        -L$mkOpenblasLib -lopenblas
+      libraries, it is both multithreaded and optimized for multiple platforms.
+      For using OpenBLAS library you should use the linker flags:
+        -L$mkOpenblasLib -lopenblas
+      By default the library run in single-thread mode, but you can set the number of
+      threads for the multi-thread mode using the proper environment variable:
+        export OPENBLAS_NUM_THREADS=4
+      or using the proper function to control the number of threads at runtime
+        void openblas_set_num_threads(int num_threads);
+      Loading this module two utility commands are provided:
+        openblas_corename - Return the name of the current architecture
+        openblas_numcores - Return the number of physical cores of cpu
+      These command can be used to set the optimal value of the OPENBLAS_NUM_THREADS
+      variable for your architecture:
+        export OPENBLAS_NUM_THREADS=$(openblas_numcores)
+*/
 
 int main() {
     // Defining Matrix dimensions
