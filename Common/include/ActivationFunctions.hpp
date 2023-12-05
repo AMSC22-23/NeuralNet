a9671aad52a4bb12053b1b9055d90b2a592247fe
nicola chiarani <nicola.chiarani@gmail.com>
2023-12-05 22:17:17 +0100

    Activation Functions declarations

---
+++ Common/include/ActivationFunctions.hpp
@@ -0,0 +1,30 @@
+#ifndef ACTIVATION_FUNCTIONS_HPP
+#define ACTIVATION_FUNCTIONS_HPP
+
+#include <cmath>
+
+template <typename T>
+inline T linearActivation(const T &input);
+
+template <typename T>
+inline T linearActivationDerivative(const T &input);
+
+template <typename T>
+inline T sigmoidActivation(const T &input);
+
+template <typename T>
+inline T sigmoidActivationDerivative(const T &input);
+
+template <typename T>
+inline T tanhActivation(const T &input);
+
+template <typename T>
+inline T tanhActivationDerivative(const T &input);
+
+template <typename T>
+inline T ReLuActivation(const T &input);
+
+template <typename T>
+inline T ReLuActivationDerivative(const T &input);
+
+#endif
